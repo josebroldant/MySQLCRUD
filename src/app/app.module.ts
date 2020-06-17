851f4ced377ab09768ce4f7f512923bda1ebf90d
unknown <josebroldant@gmail.com>
2020-06-17 17:08:08 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
